e7376ece77bcaf2e2d5e6acde326fad04dcf5b2c
Kiruthickrosan <155445031+Kiruthickrosan@users.noreply.github.com>
2024-04-12 13:05:13 +0530

    Add files via upload

---
+++ Header.php
@@ -0,0 +1,22 @@
+<div id="title">
+    <h1><span style="color: tomato;">MEN'S Celebration</span></h1>
+  </div>
+  
+ 
+<div class="container">
+    <ul id="navCircle">
+
+      <li> <h4> <a class="active" href="index.php">  <span style="color: yellow">  Home </span> </a> </h4> </li>
+
+      <li>  <h4> <a href="Category.php">  <span style="color: yellow"> Category </span> </a> <h4> </li>
+
+      <li> <h4> <a href="Products.php">  <span style="color: yellow"> Products </span> </a> </h4> </li>
+
+      <li> <h4> <a href="Offers.php">  <span style="color: yellow"> Offers </span> </a> </h4> </li>
+
+      <li> <h4> <a href="Register.php">  <span style="color: yellow ">  Register </span> </a> </h4> </li>
+
+      <li> <h4> <a href="Contact.php">  <span style="color: yellow">  Contact </span> </a> </h4> </li>
+    </ul>
+
+  </div>
---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Online Clothing Store</title>
+<link href="style.css" rel="stylesheet" type="text/css" />
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<style type="text/css">
+<!--
+.style8 {font-size: 20px;} 
+.style9 {font-size: 95%; font-weight: bold; color: tomato; font-family: Verdana, Arial, Helvetica, sans-serif; }
+-->
+</style>
+</head>
+<body>
+<div id="wrapper">
+
+  <?php
+  include "Header.php";
+  ?>
+
+  <div id="content">
+    <h1><span style="color:tomato"> Welcome To Our Online Store. . .</span></h1>
+    <table width="100%" border="0" cellspacing="5" cellpadding="3">
+      <tr>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <td><p><img src="img/Jean.jpg" alt="box" width="100" height="100" hspace="10" align="left" class="imgleft" title="box" />
+        </p></td>
+        <td><p><img src="img/shirt.jpg" alt="box" width="100" height="100" hspace="10" align="left" class="imgleft" title="box" />
+        </p></td>
+        <td><p><img src="img/t-shirt.jpg" alt="box" width="100" height="100" hspace="10" align="left" class="imgleft" title="box" /></p></td>
+      </tr>
+      <tr>
+        <td height="30" bgcolor="#BCE0A8"><div align="center" class="style8">Jeans</div></td>
+        <td bgcolor="#BCE0A8"><div align="center" class="style8">Shirts</div></td>
+        <td bgcolor="#BCE0A8"><div align="center" class="style8" >T-Shirts</div></td>
+      </tr>
+    </table>
+
+ <ul style="list-style-type:disc" >
+
+<li> <span style="color:tomato"> <h2 align="left"> Only cash on Delievery  </h2> </span> </li>
+<p>&nbsp;</p>
+<li> <h2 align="left"> Delievered within 48 Hours </h2> </li>
+<p>&nbsp;</p>
+<li> <h2 align="left"> 24*7 Customer services </h2> </li>
+<p>&nbsp;</p>
+<li> <h2 align="left"> Free delivery on orders over Rs:1000</h2> </li>
+<p>&nbsp;</p>
+<li> <h2 align="left">Always 100% Authentic Brands! </h2> </li>
+<p>&nbsp;</p>
+<li> <h2 align="left"> Shopping in the comfort of your home </h2> </li>
+<p>&nbsp;</p>
+<li> <h2 align="left"> You'll never know what you've been missing until you shop with us </h2> </li>
+
+</ul>
+
+     <p>&nbsp;</p>
+      <p>&nbsp;</p>
+       <p>&nbsp;</p>
+        <p>&nbsp;</p>
+         <p>&nbsp;</p>
+          <p>&nbsp;</p>
+  </div>
+ <?php
+ include "Right.php";
+ ?>
+  <div style="clear:both;"></div>
+   <?php
+ include "Footer.php";
+ ?>
+</div>
+</body>
+</html>
